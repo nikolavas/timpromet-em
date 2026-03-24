d2e8b09b2cd5a18fdff8b126a2d29e5d0af8a68b
Nikola <nikolavas@gmail.com>
2026-03-24 17:38:59 +0100

    Update env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,7 +1,7 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-03-22'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'timpromet-em'
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'dummy-project-id'
 
